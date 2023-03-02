076a64b2dc84830b3f6a28291b841511e0b11c8f
danidzah <danielaoliveirah01@gmail.com>
2023-03-02 00:08:39 -0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
-
+import Navbar from './components/estaticos/navbar/Navbar';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-    <>
+      <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+      <Routes> // Antigo Switch
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route/>
+      </Routes>
+      </div>
       <Footer />
-    </>
-  );
+      </Router>
+      )
+  
 }
 
 export default App;
