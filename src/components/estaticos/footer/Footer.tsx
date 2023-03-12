2340366cfa9d4b0e6e6cd15410f245991ea11b34
danidzah <danielaoliveirah01@gmail.com>
2023-03-12 20:28:26 -0300

    navbar/footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -4,41 +4,62 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Grid } from '@material-ui/core';
 import {Box} from '@mui/material';
-import './Footer.css';
+import './Footer.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/TokensReducer';
+import { useNavigate } from 'react-router-dom';
+import { addToken } from '../../../store/tokens/Actions';
 
 function Footer() {
+    
+        const token = useSelector<TokenState, TokenState["tokens"]>(
+            (state) => state.tokens
+        );
+        let navigate = useNavigate();
+        const dispatch = useDispatch();
+    
+        function goLogout() {
+            dispatch(addToken(''));
+            alert("Usuário deslogado");
+            navigate("/login");
+        }
+    
+        var footercomponent
+        if(token !=""){
+            footercomponent =  <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid alignItems="center" item xs={12}>
+                <Box className='box1'>
+                    <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                        <Typography variant="h5" align="center" gutterBottom className='texto'>Siga-nos nas redes sociais </Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <a href="https://www.facebook.com/evandro.miranda.716" target="_blank">
+                            <FacebookIcon className='redes' />
+                        </a>
+                        <a href="https://www.instagram.com/evandrera13/" target="_blank">
+                            <InstagramIcon className='redes' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/evandro-miranda-92172070/" target="_blank">
+                            <LinkedInIcon className='redes' />
+                        </a>
+                    </Box>
+                </Box>
+                <Box className='box2'>
+                    <Box paddingTop={1}>
+                        <Typography variant="subtitle2" align="center" gutterBottom className='texto' >© 2020 Copyright:</Typography>
+                    </Box>
+                    <Box>
+                        <a target="_blank" href="Evandro Miranda">
+                            <Typography variant="subtitle2" gutterBottom className='texto' align="center">Evandro.G.Miranda</Typography>
+                        </a>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+        }
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/profile.php?id=100005087598711" target="_blank">
-                                <FacebookIcon className='redes' />
-                            </a>
-                            <a href="https://www.instagram.com/danidzah/" target="_blank">
-                                <InstagramIcon className='redes' />
-                            </a>
-                            <a href="https://www.linkedin.com/in/danidzah/" target="_blank">
-                                <LinkedInIcon className='redes' />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box className="box2">
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos'>© 2023 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">Daniela Oliveira</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+        {footercomponent}
         </>
     )
 }
